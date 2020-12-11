22de871b5508bc1da95d6e503cd2d2442d035f73
Paridhi <4paridhi@gmail.com>
2020-12-11 17:39:49 +0530

    modified

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -1,28 +1,23 @@
 #include<iostream>
 using namespace std;
-void left_rotate_array(int arr[],int n){
-    int temp=arr[0];
-    int i;
-    for(i=0;i<n-1;i++){
-        arr[i]=arr[i+1];
+void rotate_array(int arr[],int n,int d){
+    for(int j=1;j<=d;j++){
+        int temp=arr[0];
+        int i;
+        for(i=0;i<n;i++){
+            arr[i]=arr[i+1];
+        }
+        arr[n-1]=temp;
     }
-    arr[i]=temp;
 }
-void rotate_array(int arr[],int n,int low,int high,int d){
-   for(int i=0;i<d;i++){
-    left_rotate_array(arr,n);
 
-   }
 
-}
 int main(){
-    int arr[]={1,2,9,4,5};
+    int arr[]={1 ,2, 3, 4 ,5, 6, 7};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int low=0;
-    int high=n-1;
     int d;
     cin>>d;
-    rotate_array(arr,n,low,high,d);
+    rotate_array(arr,n,d);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
